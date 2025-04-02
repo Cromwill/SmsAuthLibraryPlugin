c3e62e179ff3776d14367d3e9b2eaed9a51efcb4
Kulachenko Aleks <hitsugayatru@gmail.com>
2025-04-02 11:02:08 +0300

    add new define to rotate mechanics

--- Packages/com.agava.smsauth/Runtime/UIHandler/WindowPresenters/OrientationСhangeWindowPresenter.cs
+++ Packages/com.agava.smsauth/Runtime/UIHandler/WindowPresenters/OrientationСhangeWindowPresenter.cs
@@ -14,9 +14,7 @@ namespace Agava.Wink
         private IInternetChecker _internetChecker;
         private GameOrientation _gameOrientation;
         private Coroutine _waitPhoneRotateCoroutine;
-#if UNITY_EDITOR
-        private bool _needChangeOrientation = false;
-#endif
+
         public void Construct(GameOrientation gameOrientation, IInternetChecker internetChecker)
         {
             _gameOrientation = gameOrientation ?? throw new ArgumentNullException(nameof(gameOrientation));
@@ -24,14 +22,6 @@ namespace Agava.Wink
 
             _orientationСhangeAnimation.Construct();
         }
-
-#if UNITY_EDITOR
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-                _needChangeOrientation = true;
-        }
-#endif
 
         public override void Enable()
         {
@@ -54,16 +44,13 @@ namespace Agava.Wink
             }
 
             _orientationСhangeAnimation.StopAnimation();
-#if UNITY_EDITOR
-            _needChangeOrientation = false;
-#endif
         }
 
         private IEnumerator WaitRotatePhone()
         {
             _gameOrientation.SetLandscapeOrientation();
-#if UNITY_EDITOR
-            yield return new WaitUntil(() => _needChangeOrientation);
+#if UNITY_EDITOR && TEST_CHANGE_ORIENTATION
+            yield return new WaitForSeconds(2);
 #else
             while(_gameOrientation.ChangedToLandscape == false)
         {
